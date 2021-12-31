774a616cc5aaf24cad401a72910ef5668aa6d9ec
ManuCF <manuel.spk@gmail.com>
2021-12-31 05:05:47 +0100

    - First commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # MidiMasterClock
 Clock made in arduino to synchronize the tempo of synthesizers. Designed for Pocket Operator (and compatible)
+
+Midi Master Clock, is a project based on https://github.com/ejlabs/arduino-midi-sync. 
+
+In this case, it has been modified to be used with 8-segment displays, through multiplexing with a shift register, making use of interrupts with the Arduino Timer2.
+
+This project has three parts.
+
+- Electronic assembly.
+- Microcontroller programming.
+- 3D design of the cover.
---
+++ SourceCode/Arduino_Master_Sync.ino
@@ -0,0 +1,392 @@
+/*
+ * Arduino Midi Master Clock v0.2
+ * MIDI master clock/sync/divider for MIDI instruments, Pocket Operators and Korg Volca.
+ * by Eunjae Im https://ejlabs.net/arduino-midi-master-clock
+ *
+ *Modified by Manuel Canamero at: https://github.com/manuelspk/MidiMasterClock
+ *
+ * Required library
+ *    TimerOne https://playground.arduino.cc/Code/Timer1
+ *    Encoder https://www.pjrc.com/teensy/td_libs_Encoder.html
+ *    MIDI https://github.com/FortySevenEffects/arduino_midi_library
+ *******************************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *******************************************************************************
+ */
+
+
+ //Defines#######################################################################
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <TimerOne.h>
+#include <EEPROM.h>
+#include "Encoder.h"
+#include <MIDI.h>
+
+#define data 9
+#define clock 10
+#define diplay1 11
+#define diplay2 12
+#define diplay3 A1
+
+#define LED_PIN1 7 // Tempo LED
+#define SYNC_OUTPUT_PIN 6 // Audio Sync Digital Pin
+#define SYNC_OUTPUT_PIN2 8 // 2nd Audio Sync Pin
+#define BUTTON_START 5 // Start/Stop Push Button
+
+#define CLOCKS_PER_BEAT 24 // MIDI Clock Ticks
+#define AUDIO_SYNC 12 // Audio Sync Ticks
+#define AUDIO_SYNC2 12 // 2nd Audio Sync Ticks
+
+#define MINIMUM_BPM 20
+#define MAXIMUM_BPM 300
+
+#define BLINK_TIME 4 // LED blink time
+
+volatile int  blinkCount = 0,
+              blinkCount2 = 0,
+              AudioSyncCount = 0,
+              AudioSyncCount2 = 0;
+
+volatile byte primerDigito;
+volatile byte segundoDigito;
+volatile byte tercerDigito;
+
+long intervalMicroSeconds,
+      bpm,
+      audio_sync2;
+
+boolean playing = false,
+      sync_editing = false,
+      display_update = false;
+
+Encoder myEnc(2, 3); // Rotary Encoder Pin 2,3 
+
+// use binary notation to discribe our number layouts
+byte zero  = B11101110;
+byte one   = B10000010;
+byte two   = B10111100;
+byte three = B10111010;
+byte four  = B11010010;
+byte five  = B01111010;
+byte six   = B01111110;
+byte seven = B10100010;
+byte eight = B11111110;
+byte nine  = B11111010;
+byte none  = B00000000;
+
+//function called by Timer2 interrupts.
+ISR(TIMER2_OVF_vect)
+{
+  multiplexDisplay();
+}
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+void setup(void) {
+  pinMode(clock, OUTPUT); // make the clock pin an output
+  pinMode(data, OUTPUT); // make the data pin an output3
+  pinMode(diplay1, OUTPUT);
+  pinMode(diplay2, OUTPUT);
+  pinMode(diplay3, OUTPUT);
+
+  SREG = (SREG & 0b01111111); //Desabilitar interrupciones
+  TIMSK2 = TIMSK2|0b00000001; //Habilita la interrupcion por desbordamiento
+  TCCR2B = 0b00000110; //Configura preescala para que FT2 sea de 7812.5Hz
+  SREG = (SREG & 0b01111111) | 0b10000000; //Habilitar interrupciones //Desabilitar interrupciones
+  
+  Serial.begin(9600);
+  MIDI.begin(); // MIDI init
+  MIDI.turnThruOff();
+
+  bpm = EEPROMReadInt(0);
+  if (bpm > MAXIMUM_BPM || bpm < MINIMUM_BPM) {
+    bpm = 120;
+  }
+  audio_sync2 = EEPROMReadInt(3);
+  if (audio_sync2 > 64 || audio_sync2 < 2) {
+    audio_sync2 = 12;
+  }
+   
+  Timer1.initialize(2000000);
+  Timer1.attachInterrupt(sendClockPulse);  
+
+  pinMode(BUTTON_START,INPUT_PULLUP);
+  pinMode(LED_PIN1, OUTPUT);
+  
+}
+
+void EEPROMWriteInt(int p_address, int p_value)
+     {
+     byte lowByte = ((p_value >> 0) & 0xFF);
+     byte highByte = ((p_value >> 8) & 0xFF);
+
+     EEPROM.write(p_address, lowByte);
+     EEPROM.write(p_address + 1, highByte);
+     }
+
+unsigned int EEPROMReadInt(int p_address)
+     {
+     byte lowByte = EEPROM.read(p_address);
+     byte highByte = EEPROM.read(p_address + 1);
+
+     return ((lowByte << 0) & 0xFF) + ((highByte << 8) & 0xFF00);
+}
+
+void bpm_display() { 
+  updateBpm();
+  EEPROMWriteInt(0,bpm);  
+  display_update = false;
+}
+
+void sync_display() {
+  EEPROMWriteInt(3,audio_sync2);
+  
+  int sync_current;
+  sync_current = audio_sync2 - 12;  
+  
+  if (sync_current < 0) {    
+    sync_current = abs(sync_current);
+  } else if (sync_current > 0) {
+    sync_current = -sync_current;
+  }
+}
+
+void startOrStop() {
+  if (!playing) {
+    MIDI.sendRealTime(midi::Start);
+  } else {
+    all_off();
+    MIDI.sendRealTime(midi::Stop);
+  }
+  playing = !playing;
+}
+
+int oldPosition;
+
+void loop(void) {
+  byte i = 0;
+  byte p = 0;
+
+  setDisplayNumber(1, bpm % 10);
+  setDisplayNumber(2, (bpm / 10) % 10);
+  setDisplayNumber(3, (bpm / 100) % 10);
+  
+  if (digitalRead(BUTTON_START) == LOW) {
+    startOrStop();
+    delay(300); // ugly but just make life easier, no need to check debounce
+  }
+  
+  int newPosition = (myEnc.read()/4);
+  if (newPosition != oldPosition) {    
+    if (oldPosition < newPosition) {
+      i = 2;
+    } else if (oldPosition > newPosition) {
+      i = 1;
+    }
+    oldPosition = newPosition;
+  }
+  
+  if (!sync_editing) {      
+      if (i == 2) {
+        bpm++;
+        if (bpm > MAXIMUM_BPM) {
+          bpm = MAXIMUM_BPM;
+        }
+        bpm_display();          
+      } else if (i == 1) {
+        bpm--;
+        if (bpm < MINIMUM_BPM) {
+          bpm = MINIMUM_BPM;
+        }
+        bpm_display();
+      } else if (p == 1) {
+        //rotary.resetPush();
+        sync_display();
+        sync_editing = true;
+      }
+  } else  { // 2nd jack audio sync speed
+      if (p == 1) {      
+        bpm_display();
+        sync_editing = false;
+      } else if (i == 1) {      
+        audio_sync2++;
+        if (audio_sync2 > 64) { audio_sync2 = 64; }
+        sync_display();
+      } else if (i == 2) {
+        audio_sync2--;
+        if (audio_sync2 < 2) { audio_sync2 = 2; }
+        sync_display();
+      }      
+  }
+}
+
+void all_off() { // make sure all sync, led pin stat to low
+  digitalWrite(SYNC_OUTPUT_PIN, LOW);
+  digitalWrite(SYNC_OUTPUT_PIN2, LOW);
+  digitalWrite(LED_PIN1, LOW);
+}
+
+void sendClockPulse() {  
+  MIDI.sendRealTime(midi::Clock); // sending midi clock
+  
+  if (playing) {  
+  
+  blinkCount = (blinkCount + 1) % CLOCKS_PER_BEAT;
+  blinkCount2 = (blinkCount2 + 1) % (CLOCKS_PER_BEAT / 2);
+  AudioSyncCount = (AudioSyncCount + 1) % AUDIO_SYNC;
+  AudioSyncCount2 = (AudioSyncCount2 + 1) % audio_sync2;
+
+  if (AudioSyncCount == 0) {
+      digitalWrite(SYNC_OUTPUT_PIN, HIGH); 
+  } else {        
+    if (AudioSyncCount == 1) {     
+      digitalWrite(SYNC_OUTPUT_PIN, LOW);
+    }
+  }  
+
+  if (AudioSyncCount2 == 0) {
+      digitalWrite(SYNC_OUTPUT_PIN2, HIGH);
+  } else {        
+    if (AudioSyncCount2 == 1) {
+      digitalWrite(SYNC_OUTPUT_PIN2, LOW);
+    }
+  }
+  
+  if (blinkCount == 0) {
+      digitalWrite(LED_PIN1, HIGH);      
+  } else {
+     if (blinkCount == BLINK_TIME) {
+       digitalWrite(LED_PIN1, LOW);      
+     }
+  }
+  } // if playing
+}
+
+void updateBpm() { // update BPM function (on the fly)
+  long interval = 60L * 1000 * 1000 / bpm / CLOCKS_PER_BEAT;  
+  Timer1.setPeriod(interval);
+}
+
+void multiplexDisplay() 
+{ 
+  if (digitalRead(diplay1) == LOW) 
+  {
+    shiftOut(data, clock, LSBFIRST, segundoDigito);
+    digitalWrite(diplay1, HIGH);
+    digitalWrite(diplay2, LOW);
+    digitalWrite(diplay3, HIGH);
+  }
+  else if(digitalRead(diplay2) == LOW)
+  {
+    shiftOut(data, clock, LSBFIRST, tercerDigito);
+    digitalWrite(diplay1, HIGH);
+    digitalWrite(diplay2, HIGH);
+    digitalWrite(diplay3, LOW);
+  }
+  else if(digitalRead(diplay3) == LOW)
+  {
+    shiftOut(data, clock, LSBFIRST, primerDigito);
+    digitalWrite(diplay1, LOW);
+    digitalWrite(diplay2, HIGH);
+    digitalWrite(diplay3, HIGH);
+  }
+}
+
+void setDisplayNumber(int position, int number)
+{
+  switch (number) 
+  {
+    case 1:
+      if (position == 1)
+        primerDigito=one;
+      if (position == 2)
+        segundoDigito=one;
+      if (position == 3)
+        tercerDigito=one;
+      break;
+    case 2:
+      if (position == 1)
+        primerDigito=two;
+      if (position == 2)
+        segundoDigito=two;
+      if (position == 3)
+        tercerDigito=two;
+      break;
+    case 3:
+      if (position == 1)
+        primerDigito=three;
+      if (position == 2)
+        segundoDigito=three;
+      if (position == 3)
+        tercerDigito=three;
+      break;
+    case 4:
+      if (position == 1)
+        primerDigito=four;
+      if (position == 2)
+        segundoDigito=four;
+      if (position == 3)
+        tercerDigito=four;
+      break;
+    case 5:
+      if (position == 1)
+        primerDigito=five;
+      if (position == 2)
+        segundoDigito=five;
+      if (position == 3)
+        tercerDigito=five;
+      break;
+    case 6:
+      if (position == 1)
+        primerDigito=six;
+      if (position == 2)
+        segundoDigito=six;
+      if (position == 3)
+        tercerDigito=six;
+      break;
+    case 7:
+      if (position == 1)
+        primerDigito=seven;
+      if (position == 2)
+        segundoDigito=seven;
+      if (position == 3)
+        tercerDigito=seven;
+      break;
+    case 8:
+      if (position == 1)
+        primerDigito=eight;
+      if (position == 2)
+        segundoDigito=eight;
+      if (position == 3)
+        tercerDigito=eight;
+      break;
+    case 9:
+      if (position == 1)
+        primerDigito=nine;
+      if (position == 2)
+        segundoDigito=nine;
+      if (position == 3)
+        tercerDigito=nine;
+      break;
+    case 0:
+      if (position == 1)
+        primerDigito=zero;
+      if (position == 2)
+        segundoDigito=zero;
+      if (position == 3)
+        tercerDigito=zero;
+      break;
+  }
+}
